239adb4dfd28549f5b78d80b0a2f14bbb3936dfb
Alex Lin <allienx@hashmail.cc>
2022-08-16 16:34:08 -0700

    add ip address server side props

--- apps/ip-taco/pages/_app.tsx
+++ apps/ip-taco/pages/_app.tsx
@@ -1,10 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { theme } from 'config/theme/theme'
+import Head from 'next/head'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>ip taco</title>
+      </Head>
+
       <Component {...pageProps} />
     </ChakraProvider>
   )
--- apps/ip-taco/pages/index.tsx
+++ apps/ip-taco/pages/index.tsx
@@ -1,16 +1,64 @@
+import '@fontsource/jetbrains-mono/400.css'
 import '@fontsource/jetbrains-mono/700.css'
 
-import { Text } from '@chakra-ui/react'
-
-import type { NextPage } from 'next'
+import { CheckIcon, CopyIcon } from '@chakra-ui/icons'
+import {
+  Box,
+  Flex,
+  IconButton,
+  Text,
+  Tooltip,
+  useClipboard,
+} from '@chakra-ui/react'
+import type { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import { PageHeader, PageWrapper } from 'ui'
 
-const HomePage: NextPage = () => {
+export interface IndexPageProps {
+  ipAddress: string
+}
+
+export const getServerSideProps: GetServerSideProps<IndexPageProps> = async (
+  context,
+) => {
+  const { req } = context
+  const ipAddress = (req.headers['x-forwarded-for'] ||
+    req.socket.remoteAddress ||
+    '') as string
+
+  return {
+    props: {
+      ipAddress,
+    },
+  }
+}
+
+export default function HomePage({
+  ipAddress,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const { hasCopied, onCopy } = useClipboard(ipAddress)
+
   return (
     <PageWrapper maxWidth={640}>
       <PageHeader title="ip taco" />
+
+      <Flex alignItems="center" justifyContent="center">
+        <Box bgColor="gray.100" borderRadius={4} p={4}>
+          <Text fontFamily="mono" fontSize="2xl">
+            {ipAddress}
+          </Text>
+        </Box>
+
+        <Tooltip label={!hasCopied ? 'Copy to clipboard' : ''}>
+          <IconButton
+            aria-label="Copy to clipboard"
+            colorScheme={hasCopied ? 'green' : 'orange'}
+            icon={hasCopied ? <CheckIcon /> : <CopyIcon />}
+            ml={5}
+            variant="outline"
+            onClick={!hasCopied ? onCopy : undefined}
+          />
+        </Tooltip>
+      </Flex>
     </PageWrapper>
   )
 }
-
-export default HomePage
