fd07ac42f131c3758381ba319574514b166563b8
Alex Lin <allienx@hashmail.cc>
2022-08-16 07:53:17 -0700

    add chakra-ui

--- packages/ui/layout/PageHeader.tsx
+++ packages/ui/layout/PageHeader.tsx
@@ -1,5 +1,5 @@
 import { Flex, FlexProps, Heading } from '@chakra-ui/react'
-import { ReactNode } from 'react'
+import type { ReactNode } from 'react'
 
 interface PageHeaderProps extends FlexProps {
   icon?: ReactNode
