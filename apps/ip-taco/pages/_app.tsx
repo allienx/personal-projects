cfca3fdc2506512010fa84abf78733b3ceabb30b
Alex Lin <allienx@hashmail.cc>
2023-05-22 23:35:14 -0700

    [ip-taco] Fix ip taco theme import

--- apps/ip-taco/pages/_app.tsx
+++ apps/ip-taco/pages/_app.tsx
@@ -2,9 +2,8 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { createQueryClient } from 'config/create-query-client'
 import type { AppProps } from 'next/app'
-import { createTheme } from 'config/theme/create-theme'
+import { theme } from 'config/theme/theme'
 
-const theme = createTheme()
 const queryClient = createQueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
