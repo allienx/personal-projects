bbb8a9843d6990c69241b6b4080eca9d738579c4
Alex Lin <allienx@hashmail.cc>
2023-06-19 22:34:51 -0700

    [ttp-appointments] Remove dev server proxy

--- apps/ttp-appointments/src/http/ttp-api.ts
+++ apps/ttp-appointments/src/http/ttp-api.ts
@@ -1,39 +1,25 @@
-import { EnvVars } from 'src/config/env-vars'
-
 export default class TtpApi {
   static locationsUrl() {
-    return EnvVars.isProd
-      ? 'https://d7gqolvzu4eus4vys5x75lytpm0fgzxh.lambda-url.us-east-2.on.aws'
-      : '/api/locations'
+    return 'https://d7gqolvzu4eus4vys5x75lytpm0fgzxh.lambda-url.us-east-2.on.aws'
   }
 
   static slotsUrl() {
-    return EnvVars.isProd
-      ? 'https://me63q6uiwxolyxtkgk75naatxy0jfmsa.lambda-url.us-east-2.on.aws'
-      : '/api/slots'
+    return 'https://me63q6uiwxolyxtkgk75naatxy0jfmsa.lambda-url.us-east-2.on.aws'
   }
 
   static userSlotsUrl() {
-    return EnvVars.isProd
-      ? 'https://pykmienm6mzkuxgdmvgtztw4dy0qzlmh.lambda-url.us-east-2.on.aws'
-      : '/api/user-slots'
+    return 'https://pykmienm6mzkuxgdmvgtztw4dy0qzlmh.lambda-url.us-east-2.on.aws'
   }
 
   static userSlotCreateUrl() {
-    return EnvVars.isProd
-      ? 'https://rknra6vulmbgg6tsoh4reisvim0gmyzb.lambda-url.us-east-2.on.aws'
-      : '/api/create-user-slot'
+    return 'https://rknra6vulmbgg6tsoh4reisvim0gmyzb.lambda-url.us-east-2.on.aws'
   }
 
   static userSlotDeleteUrl() {
-    return EnvVars.isProd
-      ? 'https://fijxsxbbg3mxpr6polueq4ez2m0kkcrc.lambda-url.us-east-2.on.aws'
-      : '/api/delete-user-slot'
+    return 'https://fijxsxbbg3mxpr6polueq4ez2m0kkcrc.lambda-url.us-east-2.on.aws'
   }
 
   static userSlotUpdateUrl() {
-    return EnvVars.isProd
-      ? 'https://mbat4pivrfukhv5qpjtct2x3440ayozg.lambda-url.us-east-2.on.aws'
-      : '/api/update-user-slot'
+    return 'https://mbat4pivrfukhv5qpjtct2x3440ayozg.lambda-url.us-east-2.on.aws'
   }
 }
--- apps/ttp-appointments/vite.config.js
+++ apps/ttp-appointments/vite.config.js
@@ -9,68 +9,9 @@ export default defineConfig({
   server: {
     open: true,
     port: 3004,
-    proxy: {
-      '/api/locations': createLambdaProxy({
-        target:
-          'https://d7gqolvzu4eus4vys5x75lytpm0fgzxh.lambda-url.us-east-2.on.aws',
-      }),
-      '/api/slots': createLambdaProxy({
-        target:
-          'https://me63q6uiwxolyxtkgk75naatxy0jfmsa.lambda-url.us-east-2.on.aws',
-      }),
-      '/api/user-slots': createLambdaProxy({
-        target:
-          'https://pykmienm6mzkuxgdmvgtztw4dy0qzlmh.lambda-url.us-east-2.on.aws',
-      }),
-      '/api/create-user-slot': createLambdaProxy({
-        target:
-          'https://rknra6vulmbgg6tsoh4reisvim0gmyzb.lambda-url.us-east-2.on.aws',
-      }),
-      '/api/delete-user-slot': createLambdaProxy({
-        target:
-          'https://fijxsxbbg3mxpr6polueq4ez2m0kkcrc.lambda-url.us-east-2.on.aws',
-      }),
-      '/api/update-user-slot': createLambdaProxy({
-        target:
-          'https://mbat4pivrfukhv5qpjtct2x3440ayozg.lambda-url.us-east-2.on.aws',
-      }),
-    },
   },
 
   preview: {
-    proxy: {
-      '/api/locations': createLambdaProxy({
-        target:
-          'https://d7gqolvzu4eus4vys5x75lytpm0fgzxh.lambda-url.us-east-2.on.aws',
-      }),
-      '/api/slots': createLambdaProxy({
-        target:
-          'https://me63q6uiwxolyxtkgk75naatxy0jfmsa.lambda-url.us-east-2.on.aws',
-      }),
-      '/api/user-slots': createLambdaProxy({
-        target:
-          'https://pykmienm6mzkuxgdmvgtztw4dy0qzlmh.lambda-url.us-east-2.on.aws',
-      }),
-      '/api/create-user-slot': createLambdaProxy({
-        target:
-          'https://rknra6vulmbgg6tsoh4reisvim0gmyzb.lambda-url.us-east-2.on.aws',
-      }),
-      '/api/delete-user-slot': createLambdaProxy({
-        target:
-          'https://fijxsxbbg3mxpr6polueq4ez2m0kkcrc.lambda-url.us-east-2.on.aws',
-      }),
-      '/api/update-user-slot': createLambdaProxy({
-        target:
-          'https://mbat4pivrfukhv5qpjtct2x3440ayozg.lambda-url.us-east-2.on.aws',
-      }),
-    },
+    port: 3004,
   },
 })
-
-function createLambdaProxy({ target }) {
-  return {
-    target,
-    changeOrigin: true,
-    prependPath: false,
-  }
-}
